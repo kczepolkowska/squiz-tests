30c94dcac2453052437c47bb601831021255f84f
Kasia Czepółkowska <kasia@booste.com>
2022-12-13 22:33:21 +0100

    tests items on the list

--- cypress/e2e/add-first-element.cy.js
+++ cypress/e2e/add-first-element.cy.js
@@ -1,16 +1,16 @@
-describe("test first", () => {
-
-  it("add element", () => {
-      //visits the app
-      cy.visit("http://localhost:8001/")
-
-      //locate the elements 
-      cy.get("[type=text]").click()
-
-      //add first element to check value = empty 
-      cy.get('a[href="#"]').click()
-      
-      //first added elements to the list is empty, should have a value
-  })
-
+describe("add elemnts to the list", () => {
+it("add first element to the list", () => {
+  cy.visit("http://localhost:8001/")
+  cy.get("[type=text]").type("Test1").should("have.value", "Test1")
+  cy.get('a[href="#"]').click()
+  cy.get('[class=task__label]').should("have.text", "Test1")
+//add correct item to the list
+})
+it("add element", () => {
+  cy.visit("http://localhost:8001/")
+  cy.get("[type=text]").click()
+  cy.get('a[href="#"]').click()
+  cy.get("[class=tasks-list__empty-message]").should("have.text", "You have no tasks - get started by adding one below!")
+//add empty item to the list, the lists shoud be empty 
+})
 })
